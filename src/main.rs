69987e2633b02740bf809e27c3de48392729438b
Tyrel Souza <tyrelsouza@gmail.com>
2023-07-03 00:58:13 -0400

    not silent binary

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,7 @@ fn write_file(html: String) {
         copy_recursively("./static", "./output").expect("Could not copy static directory");
     }
     fs::write("./output/index.html", html).expect("Could not write to index.html");
+    println!("Yaml parsed, Template rendered, and all living in ./output/")
 }
 
 fn main() {
